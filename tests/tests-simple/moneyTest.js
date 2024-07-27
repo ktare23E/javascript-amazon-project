76a04461ce905aca747aa0abc8a513087cc0fccb
ktare23 <kristiankharl.tare@nmsc.edu.ph>
2024-07-27 20:14:46 +0800

    Renaming jasmine-test to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from "../scipts/utils/money.js";
+import {formatCurrency} from "../../scipts/utils/money.js";
 
 if(formatCurrency(2000.4) === '20.00'){
     console.log('passed');
